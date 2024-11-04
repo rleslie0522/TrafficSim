5603761abed2d6fffa50335f172e08b8aaea7df5
calvinearnshaw <calvin.earnshaw@outlook.com>
2024-11-04 17:11:27 +0000

    Added Pyrobosim to launch file + instructions

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 2. Type `colcon build --packages-select trafficsim`
 3. Type `source install/local_setup.bash`
 4. Type `ros2 launch trafficsim traffic_launch.py`
+
+## Resolving Pyrobosim Errors
+### No module named transforms3d
+1. Type `sudo apt install python3-transforms3d`
+2. Type `sudo apt install python3-shapely`
+3. Type `pip install trimesh adjustText astar Pyside6 --break-system-packages`
+4. Attempt the steps in **Build Instructions**.
--- launch/traffic_launch.py
+++ launch/traffic_launch.py
@@ -8,5 +8,14 @@ def generate_launch_description():
             package="trafficsim",
             output="screen",
             executable="traffic"
+        ),
+        Node(
+            package='pyrobosim_ros',  # Replace with the actual package name if different
+            executable='demo.py',  # Replace with the actual executable name for Pyrobosim
+            name='pyrobosim',  # Give the node a name
+            output='screen',  # Output logs to the screen
+            parameters=[{
+                'use_sim_time': True  # Enables the use of simulated time if needed
+            }]
         )
     ])

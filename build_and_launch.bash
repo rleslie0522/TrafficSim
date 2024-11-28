a3962af77148e89b8bafd4c6449eed6884629262
Enduriel <endur1el@protonmail.com>
2024-11-28 23:56:51 +0000

    build: fix build script not working if called outside of folder

--- build_and_launch.bash
+++ build_and_launch.bash
@@ -1,7 +1,8 @@
 #!/bin/bash
-python3 station_dataset/dataset_to_json.py
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path/../../"
+cd "$parent_path"
+python3 station_dataset/dataset_to_json.py
+cd "../../"
 cp ./src/trafficsim/core/trafficsim/prs_worldinit.py ./install/pyrobosim_ros/lib/pyrobosim_ros
 chmod +x ./install/pyrobosim_ros/lib/pyrobosim_ros/prs_worldinit.py
 colcon build --packages-select pyrobosim pyrobosim_msgs pyrobosim_ros trafficsim trafficsim_interfaces

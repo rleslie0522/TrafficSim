638e93abdea4654199cb7188816c270aac603c6e
Enduriel <endur1el@protonmail.com>
2024-11-28 22:46:01 +0000

    build: add missing required package to build script

--- build_and_launch.bash
+++ build_and_launch.bash
@@ -4,6 +4,6 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path/../../"
 cp ./src/trafficsim/core/trafficsim/prs_worldinit.py ./install/pyrobosim_ros/lib/pyrobosim_ros
 chmod +x ./install/pyrobosim_ros/lib/pyrobosim_ros/prs_worldinit.py
-colcon build --packages-select pyrobosim pyrobosim_ros trafficsim trafficsim_interfaces
+colcon build --packages-select pyrobosim pyrobosim_msgs pyrobosim_ros trafficsim trafficsim_interfaces
 source ./install/local_setup.bash
 ros2 launch trafficsim traffic_launch.py
